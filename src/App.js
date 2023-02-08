56609c632d130ded8403765240a2637e95d2789b
midori <punctualityman@outlook.com>
2023-02-08 12:48:37 -0500

    calculate points

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const calculatePoints = (amount) => {
+  let points = 0;
+  if (amount > 100) {
+    points += 2 * (amount - 100);
+  }
+  if (amount > 50) {
+    points += 1 * (Math.min(amount, 100) - 50);
+  }
+  return points;
+};
+
+const CustomerRewards = () => {
+  const [data, setData] = useState([]);
+  const [rewards, setRewards] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //Simulate API call to fetch data
+      const response = [
+        { customerId: 1, month: 'January', amount: 120 },
+        { customerId: 1, month: 'February', amount: 90 },
+        { customerId: 1, month: 'March', amount: 110 },
+        { customerId: 2, month: 'January', amount: 70 },
+        { customerId: 2, month: 'February', amount: 80 },
+        { customerId: 2, month: 'March', amount: 100 },
+      ];
+
+      setData(response);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const rewards = {};
+      data.forEach(({ customerId, amount }) => {
+        if (!rewards[customerId]) {
+          rewards[customerId] = {
+            total: 0,
+          };
+        }
+        const points = calculatePoints(amount);
+        rewards[customerId].total += points;
+      });
+      setRewards(rewards);
+    }
+  }, [data, loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Customer Id</th>
+              <th>Month</th>
+              <th>Amount</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(({ customerId, month, amount }, index) => (
+              <tr key={index}>
+                <td>{customerId}</td>
+                <td>{month}</td>
+                <td>{amount}</td>
+                <td>{calculatePoints(amount)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {!loading && (
+        <div>
+          {Object.keys(rewards).map((customerId) => (
+            <div key={customerId}>
+              <h2>Customer Id: {customerId}</h2>
+              <p>Total Points: {rewards[customerId].total}</p>
+            </div>
+          ))}
+        </div>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default CustomerRewards;
